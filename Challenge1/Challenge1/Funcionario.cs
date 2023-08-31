eaa352af13bf762340eb6222fdfc2b00ac22babd
Erick Goldberg <erick_goldberg@hotmail.com>
2023-08-31 00:14:03 -0300

    Adicionar arquivos de projeto.

---
+++ Challenge1/Challenge1/Funcionario.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Challenge1
+{
+    internal class Funcionario
+    {
+        public string Nome { get; set; }
+        public double SalarioBruto { get; set; }
+        public double Imposto { get; set; }
+
+        public double SalarioLiquido(double salarioBruto, double imposto)
+        {
+            return salarioBruto - imposto;
+        }
+
+        public double AumentarSalario(double porcentagem, double salarioLiquido)
+        {
+            return salarioLiquido += (porcentagem/100) * salarioLiquido;
+        }
+    }
+}
---
+++ Challenge1/Challenge1/Program.cs
@@ -0,0 +1,32 @@
+﻿
+using Challenge1;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        // Aula 45 - Easy
+
+        Funcionario funcionario = new Funcionario();
+
+        Console.Write("Nome: ");
+        funcionario.Nome = Console.ReadLine();
+
+        Console.Write("Salário Bruto: ");
+        funcionario.SalarioBruto = Convert.ToDouble(Console.ReadLine());
+
+        Console.Write("Imposto: ");
+        funcionario.Imposto = Convert.ToDouble(Console.ReadLine());
+
+        double salarioLiquido = funcionario.SalarioLiquido(funcionario.SalarioBruto, funcionario.Imposto);
+
+        Console.WriteLine("Funcionario: " + funcionario.Nome + ", $ " + salarioLiquido);
+
+        Console.WriteLine("Digite a porcentagem para aumentar o saláro: ");
+        double porcentagem = Convert.ToDouble(Console.ReadLine());
+
+        double salarioFinal = funcionario.AumentarSalario(porcentagem, salarioLiquido);
+
+        Console.WriteLine("Dados atualizados: " + funcionario.Nome + ", $ " + salarioFinal);
+    }
+}
